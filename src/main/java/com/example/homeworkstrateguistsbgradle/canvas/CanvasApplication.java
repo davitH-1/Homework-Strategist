dd6e973c9fa42e471c2c9e98b96a5005ccb9886c
ShahobCode <shahob.coder@gmail.com>
2026-04-10 23:37:52 -0700

    Created a new branch for the canvas link, will update name when I have access to other necessary files.

--- src/main/java/com/example/homeworkstrateguistsbgradle/canvas/CanvasApplication.java
+++ src/main/java/com/example/homeworkstrateguistsbgradle/canvas/CanvasApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
-@EnableCaching // This turns on the @Cacheable logic we added to the service
+@SpringBootApplication(scanBasePackages = "com.example.homeworkstrateguistsbgradle")
+@EnableCaching
 public class CanvasApplication {
     public static void main(String[] args) {
         SpringApplication.run(CanvasApplication.class, args);
