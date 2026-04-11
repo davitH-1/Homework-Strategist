427e08f766fd7d593b930dd9a93f969afde79939
ShahobCode <shahob.coder@gmail.com>
2026-04-11 14:46:54 -0700

    Delete backend-java/src/main/java/com/example/homeworkstrateguistsbgradle/GoogleCalendarService.java

--- backend-java/src/main/java/com/example/homeworkstrateguistsbgradle/GoogleCalendarService.java
+++
@@ -1,8 +0,0 @@
-// GoogleCalendarService.java
-package com.example.homeworkstrateguistsbgradle;
-import org.springframework.stereotype.Service;
-
-@Service
-public class GoogleCalendarService {
-    public Object getSchedule() { return "Schedule Loaded"; }
-}
